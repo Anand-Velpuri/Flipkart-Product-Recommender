19d3e1409fe988f58f54cdf9e1538a06b1f816f5
Anand-Velpuri <129038384+Anand-Velpuri@users.noreply.github.com>
2026-06-18 14:52:53 +0530

    Completed DataIngestion to AstraDB

---
+++ flipkart/data_ingestion.py
@@ -0,0 +1,30 @@
+from langchain_astradb import AstraDBVectorStore
+from langchain_huggingface import HuggingFaceEndpointEmbeddings
+from flipkart.data_converter import DataConverter
+from flipkart.config import Config
+
+
+class DataIngestion:
+    def __init__(self):
+        self.embeddings = HuggingFaceEndpointEmbeddings(model=Config.EMBEDDING_MODEL)
+        self.vector_store = AstraDBVectorStore(embedding=self.embeddings,
+                                               collection_name="flipkart_database",
+                                               api_endpoint=Config.ASTRA_DB_API_ENDPOINT,
+                                               token=Config.ASTRA_DB_APPLICATION_TOKEN,
+                                               namespace=Config.ASTRA_DB_KEYSPACE
+                                               )
+    
+    def ingest(self, load_existing=True):
+        if load_existing:
+            return self.vector_store
+        
+        docs = DataConverter("data/flipkart_product_review.csv").convert()
+
+        self.vector_store.add_documents(docs)
+
+        return self.vector_store
+    
+
+if __name__ == "__main__":
+    data_ingestion = DataIngestion()
+    data_ingestion.ingest(load_existing=False)
